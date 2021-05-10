6233efb0ad021c0ecc634e9f4770fc65203456a6
Laurince08 <lmportacio@gmail.com>
2021-05-10 10:22:43 +0800

    review #2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,16 +9,29 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   
+  searchHero
+  
   hero: Hero = {
-    id: 1,
-    name: "Laurince Hero"
+    id: 11,
+    name: "Iron Man"
   }
 
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+    console.log(this.selectedHero);
+  }
+
+  addHero(hero: Hero): void{
+    this.heroes.push(hero);
+  }
+
+
+
+
   constructor() { }
-  
-  
-
-
 
   ngOnInit(): void { 
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Iron Man' },
+    { id: 12, name: 'Hulk' },
+    { id: 13, name: 'Flash' },
+    { id: 14, name: 'Superman' },
+    { id: 15, name: 'Captain America' },
+    { id: 16, name: 'Thanos' },
+    { id: 17, name: 'Wonder Woman' },
+    { id: 18, name: 'Green Lantern' },
+    { id: 19, name: 'Batman' },
+    { id: 20, name: 'Spiderman' }
+]
